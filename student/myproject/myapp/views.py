a255a31ba88442b95fc26c1abc462a31d1c196ea
Mahmuda Khatun <mahmudak663@gmail.com>
2026-01-10 19:44:15 +0600

    complete curd

--- student/myproject/myapp/views.py
+++ student/myproject/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from myapp.models import*
 # Create your views here.
 def home(r):
@@ -12,10 +12,51 @@ def studentList(r):
     return render(r,'studentList.html', context)
 
 def studentADD(r):
-    return render(r)
+    if r.method=="POST":
+       s_Name=r.POST.get('Name')
+       s_Course=r.POST.get('Course')
+       s_Total_Marks=r.POST.get('Total_Marks')
+       s_image=r.FILES.get('image')
 
-def studentUpdate(r):
-    return render(r)
+       studentModel.objects.create(
+           Name=s_Name,
+           Course=s_Course,
+           Total_Marks=s_Total_Marks,
+           image=s_image,
+       )
+       return redirect('studentList')
+    return render(r,'studentADD.html')
 
-def studentDelete(r):
-    return render(r)
+def studentUpdate(r,id):
+    student=studentModel.objects.get(id=id)
+    if r.method=="POST":
+       s_Name=r.POST.get('Name')
+       s_Course=r.POST.get('Course')
+       s_Total_Marks=r.POST.get('Total_Marks')
+       s_image=r.FILES.get('image')
+
+       student.Name = s_Name
+       student.Course = s_Course
+       student.Total_Marks = s_Total_Marks
+
+       if s_image:
+         student.image = s_image
+
+         student.save()
+
+    #    studentModel(
+    #        id=id,
+    #        Name=s_Name,
+    #        Course=s_Course,
+    #        Total_Marks=s_Total_Marks,
+    #        image=s_image,
+    #    ).save()
+       return redirect('studentList')
+    context={
+        'student':student
+    }
+    return render (r,'studentUpdate.html', context)
+
+def studentDelete(r,id):
+    studentModel.objects.get(id=id).delete()
+    return redirect('studentList')
--- student/myproject/myproject/urls.py
+++ student/myproject/myproject/urls.py
@@ -2,11 +2,14 @@
 from django.contrib import admin
 from django.urls import path
 from myapp.views import*
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
     path('studentList/',studentList,name='studentList'),
     path('studentADD/',studentADD,name='studentADD'),
-    path('studentUpdate/',studentUpdate,name='studentUpdate'),
-    path('studentDelete/',studentDelete,name='studentDelete'),
-]
+    path('studentUpdate/<int:id>',studentUpdate,name='studentUpdate'),
+    path('studentDelete/<int:id>',studentDelete,name='studentDelete'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
